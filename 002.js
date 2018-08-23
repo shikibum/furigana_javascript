e34ebd9171ac5c9bfe1c0ae23f6ac02cc6d0bc52
shikibum <satoshikibu@gmail.com>
2018-08-23 23:04:30 +0900

    No7-11

---
+++ 002.js
@@ -0,0 +1,16 @@
+let text = 'かわうそ';
+console.log(text);
+
+let price = 100;
+let sale_price = price * 1.08;
+console.log(sale_price);
+
+let text = prompt('入力せよ');
+console.log(text);
+console.log('入力したのは' + text);
+
+let cost = prompt('価格を入力せよ');
+console.log(cost + 80);
+console.log(parseInt(cost) + 80);
+
+console.log('hello', 10, 3.5);
