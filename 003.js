b45ac9d81d8f592a9dbc014f69323fa5976c26c0
shikibum <satoshikibu@gmail.com>
2018-08-25 23:08:10 +0900

    capter1. No12 - capter2. No2

---
+++ 003.js
@@ -0,0 +1,19 @@
+console.lag('hello');
+
+let text = prompt('入力せよ');
+console.log(taxt)
+
+console.log((2 + 10) * 5);
+
+let kakaku = 100;
+let urine = kakaku * 1.08;
+console.log(urine);
+
+isNaN('4567')
+isNaN('-40')
+isNaN('1.08')
+isNaN('１２３')
+isNaN('128ax')
+
+let text = prompt('入力せよ');
+console.log(isNaN(text));
