5c2e7f50f88563606afbe7d640577aebe2f7a3eb
shikibum <satoshikibu@gmail.com>
2018-08-27 23:37:28 +0900

    Capter.2 No.6 - No.8

---
+++ 005.js
@@ -0,0 +1,23 @@
+let text = prompt('年齢は？');
+let age = parseInt(text);
+if (age < 20) {
+  console.log('未成年');
+} else if (age < 65) {
+  console.log('成人');
+} else {
+  console.log('高齢者');
+}
+
+let text = prompt('年齢は？');
+if (!isNaN(text)) {
+  let age = parseInt(text);
+  if (age < 20) {
+    console.log('未成年');
+  }
+}
+
+let text = prompt('年齢は？');
+let age = parseInt(text);
+if (age >= 6 && age <= 15) {
+  console.log('義務教育の対象');
+}
