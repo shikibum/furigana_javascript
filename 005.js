3920f7651425cef7d466c808f7ea849ae2627e4e
shikibum <satoshikibu@gmail.com>
2018-08-28 22:03:24 +0900

    Capter.2 No.9 - No.11

--- 005.js
+++ 005.js
@@ -21,3 +21,14 @@ let age = parseInt(text);
 if (age >= 6 && age <= 15) {
   console.log('義務教育の対象');
 }
+
+let text = prompt('年齢は？');
+let age = parseInt(text);
+if (age >= 5 || age >= 65) {
+  console.log('幼児と高齢者');
+}
+
+let text = prompt('年齢は？');
+if (!isNaN(text)) {
+  console.log('数字に変換可能');
+}
---
+++ 006.js
@@ -0,0 +1,32 @@
+let text = prompt('年齢は？');
+if (!isNaN(text)) {
+  let age = parseInt(text);
+  if (age < 20) {
+    console.log('未成年');
+  } else if (age < 65) {
+    console.log('成人');
+  } else {
+    console.log('高齢者')
+  }
+}
+
+let text = prompt('年齢は？');
+if (!isNaN(text)) {
+  let age = parseInt(text);
+  if (age < 20) {
+    console.log('未成年');
+    if (age >= 6 && age <= 15) {
+      console.log('(義務教育)');
+    }
+  } else if (age < 65) {
+    console.log('成人');
+  } else {
+    console.log('高齢者')
+  }
+}
+
+let text = prompt('年齢は？');
+let age = parseInt(text);
+if (age < 6) {
+  console.log('幼児')
+}
