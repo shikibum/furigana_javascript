52ab2364cead8af82fd633e55a04c4092cd94ae8
shikibum <satoshikibu@gmail.com>
2018-08-29 23:38:20 +0900

    Capter.3 No.1 - No.3

---
+++ 007.js
@@ -0,0 +1,13 @@
+let capital = 50000;
+while (capital >= 0) {
+  console.log(capital);
+  capital = capital - 5080;
+}
+
+for (let cnt = 0; cnt < 10; cnt++) {
+  console.log('カワウソ');
+}
+
+for (let cnt = 0; cnt < 10; cnt++) {
+  console.log(cnt + '頭目のカワウソ');
+}
